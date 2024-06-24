8dc09c5c52bd7dcdec8fa3df8f6c25bd11a412a0
Ann Busyhina <070196ann@gmail.com>
2024-06-24 20:28:11 +0300

    make build

---
+++ index.e8eec03b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider");e.style.position="fixed",e.style.top="50%",e.style.left="50%",e.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.e8eec03b.js.map
